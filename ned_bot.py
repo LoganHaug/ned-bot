9d084ff354176e1be06f7c38356260e13c977589
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-04-08 00:28:34 -0700

    Add basic bot functionality

---
+++ ned_bot.py
@@ -0,0 +1,64 @@
+"""ned_bot, the boi himself
+
+Features:
+    - Welcomes other users to the neighborhood
+    - Reacts with a "D" emoji to messages that already have "N" and "E" emojis
+    - Tells people to stop swearing
+"""
+# External imports
+import discord
+import yaml
+# No internal imports
+
+# Loads the yaml file
+with open('bot.yml', 'r') as file:
+    BOT_YAML = yaml.load(file, Loader=yaml.SafeLoader)
+with open('bad_word_list.yml', 'r') as file:
+    BAD_WORD_LIST = yaml.load(file, Loader=yaml.SafeLoader)
+# Grabs the guild name from .env
+GUILD = BOT_YAML['GUILD']
+# Initializes the discord bot
+NED_BOT = discord.Client()
+# Prints that the bot is ready
+@NED_BOT.event
+async def on_ready():
+    print(f'{NED_BOT.user} has connected to Discord')
+
+@NED_BOT.event
+async def on_member_join(member):
+    # general_channel = discord.utils.find()
+
+
+# Essentially just bad code that does the second feature
+@NED_BOT.event
+async def on_raw_reaction_add(payload):
+    # Checks if the emoji is an "E" emoji
+    if payload.emoji.name == '🇪':
+        # Gets the channel object from the channel id
+        channel = NED_BOT.get_channel(payload.channel_id)
+        # Gets the message that the reaction was made in
+        message = await channel.fetch_message(payload.message_id)
+        # Iterates through the reactions of the message
+        for reaction in range(0, len(message.reactions)):
+            # If the reaction of the emoji "N" was made
+            if message.reactions[reaction].emoji == '🇳':
+                # Makes sure we don't get an off by 1 error
+                if reaction + 1 <= len(message.reactions):
+                    # If the next emoji is an "E"
+                    if message.reactions[reaction + 1].emoji == '🇪':
+                        # Complete the bot's name
+                        await message.add_reaction('🇩')
+# Swear word checker
+@NED_BOT.event
+async def on_message(message):
+    message_content = message.content.split(' ')
+    for word in message_content:
+        if word in BAD_WORD_LIST['swearwords']:
+            channel = NED_BOT.get_channel(message.channel.id)
+            await channel.send(BOT_YAML['NO_SWEAR_TEXT'])
+
+# Stores the API token and the name of the guild the bot is in
+TOKEN = BOT_YAML['TOKEN']
+
+# Runs the bot using with the API token
+NED_BOT.run(TOKEN)
